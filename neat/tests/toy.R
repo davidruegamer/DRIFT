a162b5058a49acf01e1842fe1ce5833053de77f3
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2023-04-17 23:58:53 +0200

    add semi-structured neat models; fix toy example @phschiele

--- neat/tests/toy.R
+++ neat/tests/toy.R
@@ -1,5 +1,6 @@
 devtools::load_all("../")
 
+#### Data simulation
 n <- 10
 p <- 5
 y <- sort(rnorm(11))
@@ -8,6 +9,7 @@ leny <- length(y)
 X <- matrix(rnorm(n * p), ncol = p)[rep(1:n, leny),]
 y <- matrix(rep(y, each = n))
 
+#### Model types comparison
 mod <- neat(p, type = "ls")
 
 mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 25L,
@@ -20,13 +22,9 @@ mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 25L,
 
 pred <- mod %>% predict(list(X,y))
 
-mean(as.matrix(tfd_log_prob(tfd_normal(loc = 0, scale = 1), 
-                            mod(list(as.matrix(X), 
-                                     matrix(y))))))
--mean(mod$loss(y, pred)$numpy())
-
--mod$test_step(list(list(tf$constant(X, dtype = "float32"),
-                         tf$constant(y, dtype = "float32")), y))$logLik$numpy()/nrow(X)
+(logLik <- - mod$evaluate(list(as.matrix(X),
+                          matrix(y)),
+                     matrix(y))/nrow(X))
 
 
 matplot(t(matrix(pred, ncol = 11)), type = "l")
@@ -41,11 +39,16 @@ mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 500L,
 
 pred <- mod %>% predict(list(X,y))
 
+## ll
+(logLik <- - mod$evaluate(list(as.matrix(X),
+                               matrix(y)),
+                          matrix(y))/nrow(X))
+
 matplot(t(matrix(pred, ncol = 11)), type = "l")
 
 mod <- neat(p, type = "inter")
 
-mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 2500L,
+mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 1000L,
             validation_split = 0.1, view_metrics = FALSE, 
             callbacks = callback_early_stopping(patience = 250, 
                                                 monitor="val_logLik",
@@ -53,9 +56,13 @@ mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 2500L,
 
 pred <- mod %>% predict(list(X,y))
 
+(logLik <- - mod$evaluate(list(as.matrix(X),
+                               matrix(y)),
+                          matrix(y))/nrow(X))
+
 matplot(t(matrix(pred, ncol = 11)), type = "l")
 
-  #### with NAMs
+#### with NAMs
 
 mod <- sneat(p, type = "ls")
 
@@ -89,7 +96,48 @@ mod <- sneat(p, type = "inter")
 
 mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 500L,
             validation_split = 0.1, view_metrics = FALSE, 
-            callbacks = callback_early_stopping(patience = 25, 
+            callbacks = callback_early_stopping(patience = 25, monitor="val_logLik",
+                                                restore_best_weights = TRUE))
+
+pred <- mod %>% predict(list(X,y))
+
+matplot(t(matrix(pred, ncol = 11)), type = "l")
+
+#### with semi-structured NAMs
+
+mod <- sesneat(p, type = "ls")
+
+mod %>% fit(x = list(X,y), y = y, batch_size = 32L, 
+            epochs = 250L,
+            validation_split = 0.1, view_metrics = FALSE, 
+            callbacks = callback_early_stopping(
+              patience = 5, 
+              monitor="val_logLik",
+              restore_best_weights = TRUE))
+
+pred <- mod %>% predict(list(X,y))
+
+matplot(t(matrix(pred, ncol = 11)), type = "l")
+
+mod <- sesneat(p, type = "tp")
+
+mod %>% fit(x = list(X,y), y = y, batch_size = 32L, 
+            epochs = 500L,
+            validation_split = 0.1, view_metrics = FALSE, 
+            callbacks = callback_early_stopping(
+              patience = 5, 
+              monitor="val_logLik",
+              restore_best_weights = TRUE))
+
+pred <- mod %>% predict(list(X,y))
+
+matplot(t(matrix(pred, ncol = 11)), type = "l")
+
+mod <- sesneat(p, type = "inter")
+
+mod %>% fit(x = list(X,y), y = y, batch_size = 32L, epochs = 500L,
+            validation_split = 0.1, view_metrics = FALSE, 
+            callbacks = callback_early_stopping(patience = 25, monitor="val_logLik",
                                                 restore_best_weights = TRUE))
 
 pred <- mod %>% predict(list(X,y))
